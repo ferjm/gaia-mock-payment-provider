7156c5595bcc295f7f86dfc5f91fd8e1e61d5894
Fernando Jiménez <ferjmoreno@gmail.com>
2014-01-23 18:06:02 +0100

    Initial commit

---
+++ js/mockpayprovider.js
@@ -0,0 +1,8 @@
+window.addEventListener('DOMContentLoaded', function() {
+/*  document.getElementById('btSuccess').addEventListener('click', function() {
+    mozPaymentProvider.paymentSuccess();
+  });
+  document.getElementById('btError').addEventListener('click', function() {
+    mozPaymentProvider.paymentError();
+  });*/
+});
