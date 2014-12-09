86f6bbe08bf300fb5901df90e0237c6221585ff6
Fernando Jiménez Moreno <ferjmoreno@gmail.com>
2014-12-09 11:50:14 +0100

    Silent SMS screen

--- js/mockpayprovider.js
+++ js/mockpayprovider.js
@@ -1,12 +1,27 @@
 window.addEventListener('DOMContentLoaded', function() {
   var mozPaymentProvider = window.navigator.mozPaymentProvider ||
                            window.mozPaymentProvider;
-  console.log("mozPaymentProvider ", mozPaymentProvider);
+
+  var mainSection = document.getElementById('main-section');
+  var smsSection = document.getElementById('sms-section');
+
+  var silentNumbersContainer = document.getElementById('silent-numbers-container');
+  var silentNumberInput = document.getElementById('silent-number');
+  var silentNumbersList = document.getElementById('silent-numbers-list');
+
+  var smsAddress = document.getElementById('sms-address');
+  var smsBody = document.getElementById('sms-body');
+
+  var silentNumbers = {};
+
   var warning = document.getElementById('warning');
+  var info = document.getElementById('info');
+
   if (typeof mozPaymentProvider === 'undefined') {
     warning.innerHTML = 'This payment flow is supposed to be opened within a mozPay call';
     warning.classList.remove('hide');
-    return;
+    //mainSection.classList.add('hide');
+    //return;
   }
 
   // Button click handlers.
@@ -17,6 +32,42 @@ window.addEventListener('DOMContentLoaded', function() {
   document.getElementById('btError').addEventListener('click', function() {
     console.log("Error clicked");
     mozPaymentProvider.paymentFailed('NO REASON');
+  });
+  document.getElementById('btSms').addEventListener('click', function() {
+    mainSection.classList.add('hide');
+    smsSection.classList.remove('hide');
+  });
+  document.getElementById('btMain').addEventListener('click', function() {
+    mainSection.classList.remove('hide');
+    smsSection.classList.add('hide');
+  });
+  document.getElementById('btAddSilentNumber').addEventListener('click', function() {
+    var number = silentNumberInput.value;
+    if (silentNumbers[number]) {
+      return;
+    }
+    silentNumbers[number] = true;
+    var item = document.createElement('li');
+    item.textContent = number;
+    silentNumbersList.appendChild(item);
+    silentNumbersContainer.classList.remove('hide');
+    mozPaymentProvider.observeSilentSms(number, function(evt) {
+      console.log('Received silent SMS ' + evt.target.body);
+      info.classList.remove('hide');
+      info.textContent = 'Silent SMS received from ' + number + ': ' +
+                          evt.target.body;
+    });
+  });
+  document.getElementById('btSend').addEventListener('click', function() {
+    var req = mozPaymentProvider.sendSilentSms(smsAddress.value, smsBody.value);
+    req.onsuccess = function() {
+      info.classList.remove('hide');
+      info.textContent = 'SMS sent';
+    };
+    req.onerror = function() {
+      error.classList.remove('hide');
+      error.textContent = 'Error sending SMS: ' + req.error.name;
+    };
   });
 
   // service ID.
